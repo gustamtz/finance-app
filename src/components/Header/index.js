2d0e9456d62f8eca09d3b51698b2461885e72c21
Your Name <you@example.com>
2023-03-02 19:28:40 -0300

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import * as C from "./styles";
+
+const Header = () => {
+  return (
+    <C.Container>
+      <C.Header>
+        <C.Tilte> Finance App</C.Tilte>
+      </C.Header>
+    </C.Container>
+  );
+};
+
+export default Header;
